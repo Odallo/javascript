22e2eb229b8e790207c8195dfbc1346a12f315e6
Odallo <eugeneodali181@gmail.com>
2025-02-09 11:45:24 +0300

    done with the object constructors

--- remembrance.js
+++ remembrance.js
@@ -1,4 +1,4 @@
-const myObject = {
+/*const myObject = {
     property: 'Value!',
     otherProperty: 77,
     "obnoxious property": function() {
@@ -9,4 +9,25 @@ const myObject = {
 
 console.log(myObject.otherProperty);
 myObject["obnoxious property"]();
-console.log(myObject.property);
+console.log(myObject.property);*/
+
+/*const playerTwo = "Loki";
+const playerOneHealth = 100;
+const playerTwoHealth = 100;
+
+
+const playerOne = {
+  name: "Odin",
+};*/
+
+//object constructors
+function Player(name, health){
+  this.name = name;
+  this.health = health;
+  console.log(this.name);
+  console.log(this.health);
+}
+
+let player1 = new Player("Odin", 100);
+
+console.log(player1.name);
